c1f25e089e5b64d5dc75cd7a8f4d822299f3cb42
ViragSzabo <virag.szabo@student.nhlstenden.com>
2025-03-26 16:01:31 +0100

    Update 1.5

---
+++ Development/SPIH-v2/SPIH/SPIH/Resources/Authorization/AuthService.cs
@@ -0,0 +1,13 @@
+﻿using SPIH.Resources.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPIH.Resources.Authorization
+{
+    public class AuthService
+    {
+    }
+}
---
+++ Development/SPIH-v2/SPIH/SPIH/Resources/Database/User.cs
@@ -0,0 +1,8 @@
+﻿namespace SPIH.Resources.Database
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string Name { get; set; } = "";
+    }
+}
