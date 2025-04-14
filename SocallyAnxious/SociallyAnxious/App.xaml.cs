ccc8c71063628ee8545276640925d2d460c7728e
ViragSzabo <virag.szabo@student.nhlstenden.com>
2025-04-14 17:48:54 +0200

    SociallyAnxious version
    New version of the whole application as previously nothing worked.

---
+++ SocallyAnxious/SociallyAnxious/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SociallyAnxious
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
